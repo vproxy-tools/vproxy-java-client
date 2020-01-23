7f5caa32e447349c014e36a8aedb5105a2012192
K.G. Wang <wkgcass@hotmail.com>
2020-01-23 11:13:03 +0800

    automate the generation exception process

--- gen.sh
+++ gen.sh
@@ -1,1 +1,4 @@
 swagger-codegen generate --lang java --input-spec ../vproxy/doc/api.yaml --model-package vproxy.client.model --api-package vproxy.client.model --invoker-package vproxy.client -o ./
+
+git checkout .gitignore
+git checkout gradle*
